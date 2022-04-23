e561ba4ca78d52dcc332077057325f6e0289e16d
liel ferber <liel.farber@campus.technion.ac.il>
2022-04-23 23:00:36 +0300

    create my functions of RLE_List

--- RLEList.c
+++ RLEList.c
@@ -13,7 +13,7 @@ RLEList RLEListCreate()
 {
     RLEList list_p; //pointer to list
     if(!(list_p = (RLEList) malloc(SIZE_OF_RLEList))) return NULL; //did memory alocation work
-   // RLEList_t list = *list_p;
+    // RLEList_t list = *list_p;
     (*list_p).value = NULL; //reset struct
     (*list_p).sequence = 0;
     (*list_p).next = NULL;
@@ -24,4 +24,72 @@ void RLEListDestroy(RLEList list)
 {
     free(list);
 }
+
+RLEListResult RLEListAppend(RLEList list, char value)
+{
+    if(&list == NULL || value == NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    RLEList CurrNode = list->next;
+    while(CurrNode)
+    {
+        CurrNode = CurrNode->next;
+    }
+    RLEList NewNode;
+    if(!(NewNode = (RLEList) malloc(SIZE_OF_RLEList))) return RLE_LIST_OUT_OF_MEMORY;
+    NewNode->value = value;
+    CurrNode->next = NewNode;
+    return RLE_LIST_SUCCESS;
+}
+
+RLEListResult RLEListRemove(RLEList list, int index)
+{
+    if(&list == NULL || index == NULL)
+    {
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    if(index < 0 || index > RLEListSize(list))
+    {
+        return RLE_LIST_INDEX_OUT_OF_BOUNDS;
+    }
+    int counter = index;
+    RLEList CurrNode = list->next;
+    while(counter != 0)
+    {
+        CurrNode = CurrNode->next;
+        counter--;
+    }
+    CurrNode->next = NULL;
+    return RLE_LIST_SUCCESS;
+}
+
+char RLEListGet(RLEList list, int index, RLEListResult *result);
+{
+    if(&list == NULL || index == NULL)
+    {
+        *result = RLE_LIST_NULL_ARGUMENT;
+    }
+    if(index < 0 || index > RLEListSize(list))
+    {
+        *result = RLE_LIST_INDEX_OUT_OF_BOUNDS;
+    }
+    int counter = index;
+    RLEList CurrNode = list->next;
+    while(counter != 0)
+    {
+        CurrNode = CurrNode->next;
+        counter--;
+    }
+    ???????????if(CurrNode->next)
+    {
+        *result = RLE_LIST_SUCCESS;
+    }
+    else
+    {
+        return 0;
+    }
+    return CurrNode->next;
+}
+
 
